4c39de157189b66dd14884eaabb65b1c983cf143
IsaacLA <isaac.lopez.agudo@estudiantat.fib.upb>
2025-03-24 15:15:49 +0900

    add flag

--- simplifier/simplifier_main.ml
+++ simplifier/simplifier_main.ml
@@ -38,6 +38,7 @@ let _ucflag = ref false;;
 let _bnflag = ref false;;
 let _timeout = ref None;;
 let _stats = ref false;;
+let partial = ref false;;
 let f_help () = print_endline "help";;
 let set_filename fname = _fname := fname;;
 let set_raw () = _rawflag := true;;
@@ -46,6 +47,7 @@ let set_unsatcore () = _ucflag := true;;
 let set_foption opt = p_opt := opt :: !p_opt;;
 let set_timeout sec = _timeout := Some sec;;
 let set_stats () = _stats := true;;
+let set_partial () = _partial := true;;
   
 let msgUsage =
 "USAGE: simplifier [-d <TAG>|-b|-0|-t|-s] -f <filename>";;
@@ -60,6 +62,7 @@ let speclist = [
     ("-0", Arg.Unit set_raw, "Use raw z3 (Only checking the pure-part with Z3 ignoring the spat-part)");
     ("-t", Arg.Int set_timeout, "Set timeout [sec] (default:4294967295)");
     ("-s", Arg.Unit set_stats, "Reports execution stats (execution time for now)");
+
   ];;
 
 (* parser *)
